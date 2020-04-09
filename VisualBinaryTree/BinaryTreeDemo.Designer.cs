b64ba8aa3ad8df4f8fd6fa4bc722efa039cd73fc
crdrury <39220609+crdrury@users.noreply.github.com>
2020-04-09 16:36:03 -0400

    Added a label for the number entry field

--- VisualBinaryTree/BinaryTreeDemo.Designer.cs
+++ VisualBinaryTree/BinaryTreeDemo.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.inputField = new System.Windows.Forms.TextBox();
             this.treePanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.treePanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // inputField
@@ -40,17 +42,29 @@
             this.inputField.Name = "inputField";
             this.inputField.Size = new System.Drawing.Size(1264, 20);
             this.inputField.TabIndex = 0;
+            this.inputField.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // treePanel
             // 
             this.treePanel.AutoScroll = true;
             this.treePanel.AutoSize = true;
+            this.treePanel.Controls.Add(this.label1);
             this.treePanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.treePanel.Location = new System.Drawing.Point(0, 0);
             this.treePanel.Name = "treePanel";
             this.treePanel.Size = new System.Drawing.Size(1264, 661);
             this.treePanel.TabIndex = 1;
             this.treePanel.Paint += new System.Windows.Forms.PaintEventHandler(this.Paint_Event);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Location = new System.Drawing.Point(0, 648);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1264, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a number from 1-99";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // BinaryTreeDemo
             // 
@@ -62,6 +76,7 @@
             this.DoubleBuffered = true;
             this.Name = "BinaryTreeDemo";
             this.Text = "Form1";
+            this.treePanel.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -71,6 +86,7 @@
 
         private System.Windows.Forms.TextBox inputField;
         private System.Windows.Forms.Panel treePanel;
+        private System.Windows.Forms.Label label1;
     }
 }
 
